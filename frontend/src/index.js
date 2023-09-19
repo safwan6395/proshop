e46986144094d30dceeda0c78d2e54eb202b152e
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-09-19 22:32:17 +0500

    Create individual product screen and introduce some dummy backend routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,16 +7,18 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 // styles
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
-import HomeScreen from "./screens/HomeScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/products/:id' element={<ProductScreen />} />
     </Route>
   )
 );
